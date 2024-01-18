43ff86308b3eeddedca2a5503fe5e1e3afe30fc7
KunPaul <kungurtsev200@mail.ru>
2024-01-18 13:43:06 +0500

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Павел Кунгурцев on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
